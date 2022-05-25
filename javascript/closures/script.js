1bc719ac659d8f8fa495a2c79a51710031d37a0e
bodla hruthik <bodla.hruthik@betterplace.co.in>
2022-05-25 13:21:44 +0530

    added closures script

---
+++ javascript/closures/script.js
@@ -0,0 +1,18 @@
+function outer(arg1){
+    let var1 = 10
+    let x = 4
+    console.log(arguments, 'outer')
+    function inner(arg2){
+        let var2 = 20
+        x = 8
+        console.log(arguments, 'inner')
+        console.log(arg1, var1, arg2, var2, x)
+    }
+    // console.log(x)
+    return inner
+}
+
+let y = outer('param1')
+// console.log(x)
+y('param2')
+// console.log(x)
